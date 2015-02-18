d8ba954810e5436ba66730ede12ca498194a9adc
Kate <kbalducci@hotmail.com>
2015-02-18 11:23:48 -0600

    added associations and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+10.times do
+  user = User.create(first_name: Faker::Name.first_name,
+                    last_name: Faker::Name.last_name,
+                    age: rand(18...30),
+                    favorite_color: %w(red blue yellow brown green organge).sample)
+  3.times do
+    user.meals.create(name: Faker::Hacker.noun,
+                      cost: rand(5..99))
+  end
+  2.times do
+    user.pets.create(name: %w(fido fluffy foobar lilbuck beans iron mittens roofus).sample,
+                    kind: %w(fish dog cat bird turtle vampire rabbit).sample,
+                    weight: rand(14...250)) #oz
+  end
+end
